d4123ad29cb80e69ef9c56f2ae01b8f02e6da9ff
Xinru Li <x.li@math.leidenuniv.nl>
2018-03-17 15:34:12 -0600

    Correct an error that the final tree will include at least 2-steps even only one main effect is detected

--- R/SupportLookAhead.R
+++ R/SupportLookAhead.R
@@ -197,8 +197,73 @@ REmrt.fit0 <- function(mf, maxL, minbucket, minsplit, delQ, lookahead){
       cpt[[2]] <- tcpt2
     }
 
+    # continue here for the 3rd split
+
+    if (maxL > 2 & (nrow(res)==3) ) {
+      for (i in 3:maxL) {
+        Dev<- -Inf
+        tres <- NULL
+        cnode <- nodemark[ ,i]
+        len.node <- tapply(vi, cnode, length)
+        nodes <- names(len.node) [len.node >= minsplit]
+        if (length(nodes) == 0) break
+        for (j in 1:length(nodes)) {
+          pleaf.inx <- cnode == as.numeric(nodes[j])
+          for (k in 1:nmod){
+            chosenmod <- mods[pleaf.inx, k]
+            if(sapply(mods[mods.names[k]], is.numeric) == FALSE) {
+              mod.order <- rank(tapply(y[pleaf.inx],chosenmod,mean))
+              cmod.ordinal <- mod.order[as.character(chosenmod)]
+            } else {
+              mod.order <- unique(chosenmod)
+              cmod.ordinal <- chosenmod}
+            cpoints <- sort(mod.order)
+            if (length(cpoints) >= 2) {
+              for (g in 1:(length(cpoints)-1)) {
+                cnode.test <- cnode
+                cnode.test[pleaf.inx] <- ifelse( cmod.ordinal <= cpoints[g], 2*i, 2*i+1)
+                if (min(table(cnode.test[pleaf.inx])) <= minbucket) {
+                  Dev.new <- -Inf
+                } else {
+                  temp <- rebetQ(y, vi, mods = as.factor(cnode.test))
+                  Dev.new <- temp[1]
+                }
+                if (Dev.new > Dev) {
+                  Dev <- temp[1]
+                  if(sapply(mods[mods.names[k]], is.numeric) == FALSE) {
+                    tcpt <- names(mod.order[mod.order <= cpoints[g]])
+                    msplit <- paste(mods.names[k], "=", paste(names(mod.order[mod.order <= cpoints[g]]), collapse = "/"), collapse = " ")
+                  } else {
+                    tcpt <- cpoints[g]
+                    msplit <- paste(mods.names[k], "<=", tcpt, collapse = " ")
+                  }
+                  tres <- data.frame(Qb = temp[1], tau2 = temp[2],
+                                     split = msplit, mod = mods.names[k], pleaf = as.numeric(nodes[j]))
+                  tnode <- cnode.test
+                }
+              }
+            }
+          }
+        }
+
+        if (is.null(tres)) {
+          break
+        } else {
+          delta.Q <- abs(tres$Qb[1] - res$Qb[i])
+          if (delta.Q < delQ) break
+          new.node <- tnode
+          nodemark <- cbind(nodemark, new.node)
+          res <- rbind(res, tres)
+          cpt[[i]] <- tcpt
+        }
+
+
+      }
+    }
+
+
   } else {
-    for (i in 1:2) {
+    for (i in 1:maxL) {
       Dev<- -Inf
       tres <- NULL
       cnode <- nodemark[ ,i]
@@ -260,69 +325,6 @@ REmrt.fit0 <- function(mf, maxL, minbucket, minsplit, delQ, lookahead){
   }
 
 
-  # continue here for the 3rd split
-
-  if (maxL > 2 & (nrow(res)==3) ) {
-    for (i in 3:maxL) {
-      Dev<- -Inf
-      tres <- NULL
-      cnode <- nodemark[ ,i]
-      len.node <- tapply(vi, cnode, length)
-      nodes <- names(len.node) [len.node >= minsplit]
-      if (length(nodes) == 0) break
-      for (j in 1:length(nodes)) {
-        pleaf.inx <- cnode == as.numeric(nodes[j])
-        for (k in 1:nmod){
-          chosenmod <- mods[pleaf.inx, k]
-          if(sapply(mods[mods.names[k]], is.numeric) == FALSE) {
-            mod.order <- rank(tapply(y[pleaf.inx],chosenmod,mean))
-            cmod.ordinal <- mod.order[as.character(chosenmod)]
-          } else {
-            mod.order <- unique(chosenmod)
-            cmod.ordinal <- chosenmod}
-          cpoints <- sort(mod.order)
-          if (length(cpoints) >= 2) {
-            for (g in 1:(length(cpoints)-1)) {
-              cnode.test <- cnode
-              cnode.test[pleaf.inx] <- ifelse( cmod.ordinal <= cpoints[g], 2*i, 2*i+1)
-              if (min(table(cnode.test[pleaf.inx])) <= minbucket) {
-                Dev.new <- -Inf
-              } else {
-                temp <- rebetQ(y, vi, mods = as.factor(cnode.test))
-                Dev.new <- temp[1]
-              }
-              if (Dev.new > Dev) {
-                Dev <- temp[1]
-                if(sapply(mods[mods.names[k]], is.numeric) == FALSE) {
-                  tcpt <- names(mod.order[mod.order <= cpoints[g]])
-                  msplit <- paste(mods.names[k], "=", paste(names(mod.order[mod.order <= cpoints[g]]), collapse = "/"), collapse = " ")
-                } else {
-                  tcpt <- cpoints[g]
-                  msplit <- paste(mods.names[k], "<=", tcpt, collapse = " ")
-                }
-                tres <- data.frame(Qb = temp[1], tau2 = temp[2],
-                                   split = msplit, mod = mods.names[k], pleaf = as.numeric(nodes[j]))
-                tnode <- cnode.test
-              }
-            }
-          }
-        }
-      }
-
-      if (is.null(tres)) {
-        break
-      } else {
-        delta.Q <- abs(tres$Qb[1] - res$Qb[i])
-        if (delta.Q < delQ) break
-        new.node <- tnode
-        nodemark <- cbind(nodemark, new.node)
-        res <- rbind(res, tres)
-        cpt[[i]] <- tcpt
-      }
-
-
-    }
-  }
 
   list(tree = res, node.split = nodemark, cpt = cpt, data = mf)
 
